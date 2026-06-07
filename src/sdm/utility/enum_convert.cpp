23def4dda1e2ad44d9d990d152fa74872157a9a0
advisedy <advisedy@qq.com>
2026-06-07 16:06:10 +0800

    收紧 node status 0.3

--- src/sdm/utility/enum_convert.cpp
+++ src/sdm/utility/enum_convert.cpp
@@ -4,34 +4,6 @@
 
 namespace adviskv::sdm {
 
-bool convert_pb_to_node_status(pb::NodeStatus in, NodeStatus& out) {
-    switch (in) {
-        SWITCH_TYPE_EQUAL2(out, pb::NodeStatus, NODE_ONLINE, NodeStatus,
-                           ONLINE)
-        SWITCH_TYPE_EQUAL2(out, pb::NodeStatus, NODE_OFFLINE, NodeStatus,
-                           OFFLINE)
-        SWITCH_TYPE_EQUAL2(out, pb::NodeStatus, NODE_SUSPECT, NodeStatus,
-                           SUSPECT)
-        default:
-            return false;
-    }
-    return true;
-}
-
-bool convert_node_status_to_pb(NodeStatus in, pb::NodeStatus& out) {
-    switch (in) {
-        SWITCH_TYPE_EQUAL2(out, NodeStatus, ONLINE, pb::NodeStatus,
-                           NODE_ONLINE)
-        SWITCH_TYPE_EQUAL2(out, NodeStatus, OFFLINE, pb::NodeStatus,
-                           NODE_OFFLINE)
-        SWITCH_TYPE_EQUAL2(out, NodeStatus, SUSPECT, pb::NodeStatus,
-                           NODE_SUSPECT)
-        default:
-            return false;
-    }
-    return true;
-}
-
 bool convert_pb_replica_status_to_phase(pb::ReplicaStatus in, ReplicaPhase& out) {
     switch (in) {
         SWITCH_TYPE_EQUAL2(out, pb::ReplicaStatus, ADDING, ReplicaPhase,
--- src/sdm/utility/enum_convert.h
+++ src/sdm/utility/enum_convert.h
@@ -4,19 +4,5 @@
 #include "sdm/model/store.h"
 namespace adviskv::sdm {
 
-bool convert_pb_to_node_status(pb::NodeStatus in, sdm::NodeStatus& out);
-bool convert_node_status_to_pb(sdm::NodeStatus in, pb::NodeStatus& out);
 bool convert_pb_replica_status_to_phase(pb::ReplicaStatus in, ReplicaPhase& out);
-
-// ######################################################
-//  提供给外部的接口
-#define CONVERT_PB_TO_NODE_STATUS(in, out)                               \
-    {                                                                    \
-        bool res = convert_pb_to_node_status(in, out);                   \
-        if (!res) {                                                      \
-            fill_base_rsp(response, Status{StatusCode::INVALID_ARGUMENT, \
-                                           "node_status is not valid"}); \
-            return grpc::Status::OK;                                     \
-        }                                                                \
-    }
 }  // namespace adviskv::sdm
